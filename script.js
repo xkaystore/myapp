77437c86ed9bd76af4f9b985bc09dd412fd7769b
xkaystore <xkaystore@gmail.com>
2025-09-08 10:09:12 +0700

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function tambahCatatan() {
-  let input = document.getElementById("noteInput");
-  let list = document.getElementById("noteList");
-
-  if (input.value.trim() !== "") {
-    let li = document.createElement("li");
-    li.textContent = input.value;
-    list.appendChild(li);
-    input.value = "";
-  } else {
-    alert("Catatan tidak boleh kosong!");
-  }
+function sapa() {
+  alert("Selamat datang di aplikasi webku!");
 }
